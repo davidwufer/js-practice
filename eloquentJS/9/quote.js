fe569d309347c953db36d05f3930502137d7f66a
David Wu <davidcnwu@gmail.com>
2015-01-01 13:57:42 -0800

    Another solution.

--- eloquentJS/9/quote.js
+++ eloquentJS/9/quote.js
@@ -2,3 +2,6 @@ var text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
 debug(text.replace(/(')([^']+'?[^']+)(')/g, "\"$2\""));
 // → "I'm the cook," he said, "it's my job."
+
+// Another way that may not be as great?
+debug(text.replace(/(^|\W)'|'\W/g, "$1\""));
